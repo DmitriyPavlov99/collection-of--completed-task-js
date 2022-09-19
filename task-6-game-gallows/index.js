6ffbbbba027a0b994fddfa125d2f1da540b893c6
Dmitriy <dimapavlov19dp99@gmail.com>
2022-09-19 13:24:13 +0300

    done tech task

--- task-6-game-gallows/index.js
+++ task-6-game-gallows/index.js
@@ -1,44 +1,68 @@
 // Создаем массив со словами
 let words = [
-  'программа',
-  'макака',
-  'прекрасный',
-  'оладушек',
-  'автомобиль',
-  'машина',
-  'космос',
+  'ауди',
+  'мерседес',
+  'бмв',
+  'форд',
+  'мазда',
+  'киа',
+  'шкода',
+  'ниссан',
+  'лексус',
+  'сузуки',
+  'тойота',
+  'субару',
+  'тесла',
+  'порш',
 ];
-// Выбираем случайное слово
-let word = words[Math.floor(Math.random() * words.length)];
-// Создаем итоговый массив
-let answerArray = [];
-for (let i = 0; i < word.length; i++) {
-  answerArray[i] = '_';
-}
-let remainingLetters = word.length;
-// Игровой цикл
-while (remainingLetters > 0) {
-  // Показываем состояние игры
-  alert(answerArray.join(' '));
-
-  // Запрашиваем вариант ответа
-  let guess = prompt('Угадайте букву, или нажмите Отмена для выхода из игры.');
-  if (guess === null) {
-    // Выходим из игрового цикла
-    break;
-  } else if (guess.length !== 1) {
-    alert('Пожалуйста, введите одиночную букву.');
-  }else {
-    // Обновляем состояние игры
-    for (let j = 0; j < word.length; j++) {
-      if (word[j] === guess) {
-        answerArray[j] = guess;
-        remainingLetters--;
+let btn = document.querySelector('#start');
+let letters = document.querySelector('#letters');
+btn.addEventListener('click', start);
+function start() {
+  let word = words[Math.floor(Math.random() * words.length)];
+  // Создаем итоговый массив
+  let answerArray = [];
+  for (let i = 0; i < word.length; i++) {
+    answerArray[i] = '_';
+  }
+  let remainingLetters = word.length;
+  // Игровой цикл
+  let counterAttempts = 25;
+  while (remainingLetters > 0 && counterAttempts > 0) {
+    // Показываем состояние игры
+    alert(answerArray.join(' '));
+    alert(`У вас осталось ${counterAttempts} попытки`);
+    // Запрашиваем вариант ответа
+    let guess = prompt(
+      'Угадайте букву, или нажмите Отмена для выхода из игры.'
+    );
+    letters.innerHTML = guess
+    if (guess === null) {
+      // Выходим из игрового цикла
+      break;
+    } else if (guess.length !== 1) {
+      alert('Пожалуйста, введите одиночную букву.');
+      counterAttempts--;
+    } else {
+      counterAttempts--;
+      guess = guess.toLowerCase();
+      // Обновляем состояние игры
+      for (let j = 0; j < word.length; j++) {
+        if (word[j] === guess && answerArray[j] === '_') {
+          answerArray[j] = guess;
+          remainingLetters--;
+        }
       }
     }
+    // Конец игрового цикла
   }
-  // Конец игрового цикла
+
+  // Отображаем ответ и поздравляем игрока
+  alert(answerArray.join(' '));
+  if (counterAttempts > 0) {
+    alert('Игра закончена.Победа! Слово было ' + word);
+  } else {
+    alert('Можно было лучше! Было загадано слово ' + word);
+  }
 }
-// Отображаем ответ и поздравляем игрока
-alert(answerArray.join(' '));
-alert('Отлично! Было загадано слово ' + word);
+// Выбираем случайное слово
